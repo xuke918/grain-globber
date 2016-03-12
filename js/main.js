934177b08a73a768f26756f816277460e54ffc7a
xuke918 <xuke3070@gmail.com>
2016-03-12 01:12:51 -0500

    Added img src.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 var listGrains = function (grains) {
   grains.forEach(function (item) {
     document.write('<h2>' + item.name + '</h2>');
-    document.write('<img src="images/' + grains.img + '" alt="">')
-  })
-}
+    document.write('<img src="images/' + grains.img + '" alt="">');
+    document.write('desc');
+  });
+};
 
 listGrains(grains);
