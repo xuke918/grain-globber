b7410c0ec30de2b9b8a97f74575c374f3e19bc90
xuke918 <xuke3070@gmail.com>
2016-03-12 01:05:25 -0500

    Added document.write to h2 and img.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var listGrains = function (grains) {
+  grains.forEach(function (item) {
+    document.write('<h2>' + item.name + '</h2>');
+    document.write('<img src="images/' + grains.img + '" alt="">')
+  })
+}
+
+listGrains(grains);
