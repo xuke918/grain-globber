d35e8de83c21cb98455ddb1a6bf2f262cd78368a
xuke918 <xuke3070@gmail.com>
2016-03-15 17:35:15 -0400

    Deleted white space.

--- js/main.js
+++ js/main.js
@@ -4,9 +4,15 @@ $ul.addClass('grains');
 
 grains.forEach(function (grain) {
   var $li = $('<li>');
+  var $h2 = $('<h2>');
   var $img = $('<img>');
+  var $p = $('<p>');
 
   $img.attr('src', 'images/' + grain.img);
+  $h2.html(grain.name);
+  $p.html(grain.desc);
+
   $ul.append($li);
-  $li.append($img);
+  $li.append($img, $h2, $p);
+  $p.prepend($h2);
 });
